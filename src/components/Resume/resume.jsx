ef2b9c1c205510990cc8c89b34fa57fd971d743b
garrettperson <gagmac@gmail.com>
2024-04-06 16:54:15 -0700

    changed resume pdf file

--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const Resume = () => {
- return (
- <div>
- <iframe src="../public/Garrett-Gutierrez.pdf" width="100%" height="500px" />
- </div>
- );
-};
+import path from 'node:path';
+import fs from 'node:fs';
+
+const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
+const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.js');
+
+fs.copyFileSync(pdfWorkerPath, './dist/pdf.worker.js');
+
+import { Document, Page } from 'react-pdf';
+
+function Resume() {
+  return (
+    <div>
+      <Document file="../public/Garrett-Gutierrez.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+      </Document>
+    </div>
+  );
+}
+
 export default Resume;
